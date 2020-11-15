4a2497048b3fb5942379b49667c5acbe2c5f7a9c
PiotrKolecki <piotr.kolecki97@gmail.com>
2020-11-15 19:31:05 +0100

    improvements

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 [![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=3548170&assignment_repo_type=AssignmentRepo)
+
+# Features
+* logging
+    * colors
+    * visual display of roll on "progress bar"
+* Classes:
+    * Plane - uses Correction Class
+    * Environment - creates new Turbulence Events on plane
+    * Event - abstract
+        * Turbulence
+        * Correction
+* python generator used for random numbers
+
+# Sample output (without colors)
+```
+INFO[2020-11-15 19:22:48,537]: [-------------------------0-------------------------] : -0.25496701913656683
+INFO[2020-11-15 19:22:48,643]: [--------------------------0------------------------] : +6.003553489514761
+INFO[2020-11-15 19:22:48,748]: [-------------------------0-------------------------] : +1.5291945502224182
+INFO[2020-11-15 19:22:48,850]: [-------------------------0-------------------------] : +0.3371020239824274
+INFO[2020-11-15 19:22:49,052]: [-------------------------0-------------------------] : +1.7620025010862428
+INFO[2020-11-15 19:22:49,153]: [-------------------------0-------------------------] : +1.0636834851654573
+INFO[2020-11-15 19:22:49,254]: [-------------------------0-------------------------] : +0.8621912217553902
+INFO[2020-11-15 19:22:49,355]: [-------------------------0-------------------------] : +0.8893826400353904
+INFO[2020-11-15 19:22:49,388]: # Keyboard Interrupt #
+```
--- task.py
+++ task.py
@@ -1,64 +1,112 @@
-#
-#Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction.
-# (Tilt is "Roll" in this diagram https://www.novatel.com/assets/Web-Phase-2-2012/Solution-Pages/AttitudePlane.png)
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is changing with random angle with gaussian distribution (the planes is experiencing "turbuence"). 
-# Hint: "random.gauss(0, 2*rate_of_correction)"
-#With every simulation step the orentation should be corrected, correction should be applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#Delete these comments before commit!
-#
-#Good luck.
-
+from abc import ABC, abstractclassmethod
 import sys
 import random
 import time
-
-#pip install colorama
+import math
+import logging
 import colorama
+
+logging.basicConfig(
+    format='%(levelname)s[%(asctime)s]: %(message)s',
+    level=logging.INFO
+)
+
 colorama.init()
 
+
 class bcolors:
-        OKBLUE = '\033[94m'
-        OKRED = '\033[91m'
-        ENDC = '\033[0m'
+    OKBLUE = '\033[94m'
+    OKRED = '\033[91m'
+    OKGREEN = '\033[92m'
+    ENDC = '\033[0m'
+
 
 class Plane:
-  
-  def __init__(self, roll_tolerance_point = 1, roll_adjust_value = 0.1):
-    self.roll = 0 # +- 180deg
-    self.roll_tolerance_point = roll_tolerance_point #deg
-    self.roll_adjust_value = roll_adjust_value
+    def __init__(
+            self,
+            roll_tolerance_point=2.5,
+            roll_warning_point=5
+            ):
+        self.roll = 0  # +- 90 deg
+        self.previous_roll = 0
+        self.roll_tolerance_point = roll_tolerance_point
+        self.roll_warning_point = roll_warning_point
 
-  def adjust_roll(self):
-    if(plane.roll > 0):
-      plane.roll -= self.roll_adjust_value
-    else:
-      plane.roll += self.roll_adjust_value
+    def __str__(self):
+        roll_display_length = 51
+        roll_display_percent = int(self.roll * roll_display_length / 180)
+        out = "[{}0{}] : {:+}{}".format(
+            (math.floor(roll_display_length/2)+roll_display_percent)*'-',
+            (math.floor(roll_display_length/2)-roll_display_percent)*'-',
+            plane.roll,
+            bcolors.ENDC
+        )
+        if(abs(self.roll) > self.roll_warning_point):
+            return "{}{}".format(bcolors.OKRED, out)
+        else:
+            return "{}{}".format(bcolors.OKGREEN, out)
 
-  
-if __name__ == "__main__":
-  plane = Plane()
-  rate_of_correction = 0.1
-  try :
+    def correction(self):
+        if(abs(self.roll) > self.roll_tolerance_point):
+            self.roll += Correction(self.roll).adjust_roll(self.previous_roll)
+        self.previous_roll = self.roll
+
+
+def same_line_print(plane):
+    sys.stdout.write(f"\r{plane}     ")
+
+
+def random_generator(rate_of_correction):
     while True:
-      plane.roll += random.gauss(0, 2*rate_of_correction)
-      if(abs(plane.roll) > 1):
-        print("{} {} {}".format(bcolors.OKRED, plane.roll, bcolors.ENDC))
-      else:
-        print("{} {} {}".format(bcolors.OKBLUE, plane.roll, bcolors.ENDC))
-      if(abs(plane.roll) > plane.roll_tolerance_point):
-        plane.adjust_roll()
-      time.sleep(0.1)
-  except KeyboardInterrupt:
-    pass
+        n = random.gauss(0, 2*rate_of_correction)
+        yield n
+
+
+class Event(ABC):
+    def __init__(self, roll):
+        self.roll = roll
+
+    @abstractclassmethod
+    def adjust_roll(self):
+        pass
+
+
+class Correction(Event):
+    def adjust_roll(self, previus_roll):
+        if previus_roll > 0:
+            return -abs(previus_roll - self.roll) * random.random()
+        return abs(previus_roll - self.roll) * random.random()
+
+
+class Turbulence(Event):
+    def __init__(self, roll, wind):
+        super().__init__(roll)
+        self.roll += self.adjust_roll(wind)
+
+    def adjust_roll(self, wind):
+        return wind ** 2
+
+
+class Environment:
+    def __init__(self, rate_of_correction=1, wind=0.5):
+        self.rate_of_correction = rate_of_correction
+        self.gen = random_generator(self.rate_of_correction)
+        self.wind = wind
+
+    def turbuence(self, plane):
+        plane.roll += Turbulence(next(self.gen), self.wind).roll
+
+
+if __name__ == "__main__":
+    plane = Plane()
+    environment = Environment()
+
+    try:
+        while True:
+            environment.turbuence(plane)
+            plane.correction()
+            logging.info(plane)
+            # same_line_print(plane)
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        logging.info(f"{bcolors.OKRED}# Keyboard Interrupt #{bcolors.ENDC}")
